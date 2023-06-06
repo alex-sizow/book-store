d0c9c9340be1d636df85a3a66685f91346eceb0f
Alex <alex_sizow@outlook.com>
2023-06-06 23:52:46 +0300

    inicial commit

---
+++ app.vue
@@ -0,0 +1,138 @@
+<template>
+	<NuxtLayout>dgf</NuxtLayout>
+</template>
+
+<style lang="scss">
+.page-container {
+	margin-left: 120px;
+	margin-top: 1rem;
+	margin-right: 1rem;
+	padding: 1.5rem;
+	border-radius: 10px;
+	backdrop-filter: blur(6px);
+	-webkit-backdrop-filter: blur(6px);
+	box-shadow: 0 4px 16px 0 rgba(31, 38, 135, 0.37);
+}
+* {
+	padding: 0px;
+	margin: 0px;
+	border: none;
+}
+
+*,
+*::before,
+*::after {
+	box-sizing: border-box;
+}
+
+:focus,
+:active {
+	/*outline: none;*/
+}
+
+a:focus,
+a:active {
+	/* outline: none;*/
+}
+
+/* Links */
+
+a,
+a:link,
+a:visited {
+	/* color: inherit; */
+	text-decoration: none;
+	/* display: inline-block; */
+}
+
+a:hover {
+	/* color: inherit; */
+	text-decoration: none;
+}
+
+/* Common */
+
+aside,
+nav,
+footer,
+header,
+section,
+main {
+	display: block;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+	font-size: inherit;
+	font-weight: inherit;
+}
+
+ul,
+ul li {
+	list-style: none;
+}
+
+img {
+	vertical-align: top;
+}
+
+img,
+svg {
+	max-width: 100%;
+	height: auto;
+}
+
+address {
+	font-style: normal;
+}
+
+/* Form */
+
+input,
+textarea,
+button,
+select {
+	font-family: inherit;
+	font-size: inherit;
+	color: inherit;
+	background-color: transparent;
+}
+
+input::-ms-clear {
+	display: none;
+}
+
+button,
+input[type='submit'] {
+	display: inline-block;
+	box-shadow: none;
+	background-color: transparent;
+	background: none;
+	cursor: pointer;
+}
+
+input:focus,
+input:active,
+button:focus,
+button:active {
+	outline: none;
+}
+
+button::-moz-focus-inner {
+	padding: 0;
+	border: 0;
+}
+
+label {
+	cursor: pointer;
+}
+
+legend {
+	display: block;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<div class="container"><Navbar></Navbar><slot /></div>
+</template>
+
+<style lang="scss"></style>
